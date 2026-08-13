5bc950b942167965bf574d6c4c4e83c49015da8a
Delyassss <ilyassda04@gmail.com>
2026-08-13 04:14:18 +0100

    fixing the ci curl error

--- setup/dashboard.py
+++ setup/dashboard.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, render_template
+# from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder="design")
+# app = Flask(__name__, template_folder="design")
 
 
-@app.route('/', methods=['GET', 'POST'])
-def info() :
-    if request.method == 'GET' :
-        return render_template('./design/index.html')
-    return render_template('./design/index.html')
+# @app.route('/', methods=['GET', 'POST'])
+# def info() :
+#     if request.method == 'GET' :
+#         return render_template('./design/index.html')
+#     return render_template('./design/index.html')
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+#     if __name__ == '__main__':
+#         app.run(debug=True)
